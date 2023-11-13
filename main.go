fe949c0af38b909f835e3a65edf444373883ab15
lhaavel <lukas.haavel@gmail.com>
2023-11-13 11:55:08 +0200

    tegina asjuuu

--- main.go
+++ main.go
@@ -2,16 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
-func main() {
+var tpl *template.Template
 
-	http.ListenAndServe(":8080", nil)
+func main() {
+	var err error
+	tpl, err = template.ParseGlob("*.html")
+	if err != nil {
+		log.Fatalf("Error parsing templates: %v", err)
+		return
+	}
 	http.HandleFunc("/", mainPage)
 
+	http.HandleFunc("/tulemus", resultHandler)
+	err2 := http.ListenAndServe(":8080", nil)
+	if err2 != nil {
+		log.Fatal("Could not start server")
+		return
+	}
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "lala")
+	if r.URL.Path != "/" {
+		fmt.Fprint(w, "midaaaaaaaaaa")
+		return
+	}
+	tpl.ExecuteTemplate(w, "mainpage.html", nil)
+}
+
+func resultHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Fprint(w, "404 Page not found")
+	}
+	r.FormValue()
 }
