9747f6116a9ffae34be13d7cf0b7fb4d76f1fb3c
rainpraks <133800439+rainpraks@users.noreply.github.com>
2023-11-29 15:43:46 +0200

    rain

--- Functions/ScoreCalc.go
+++ Functions/ScoreCalc.go
@@ -500,7 +500,7 @@ func ScoreCalc(inputKon, inputTeh []string) ScoreSlice {
 			TechRating: techS,
 			ConRating:  conS,
 			Image:      img,
-			Link:       "https://tsenter.ee/",
+			Link:       "static/Tehniline kirjeldus - Eksentriktõmmits - Leht1.pdf",
 		}
 		list = append(list, s)
 	}
